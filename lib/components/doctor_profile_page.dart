d7268c502aefdd79d3ed4f1df0eac5a28a07beb2
Julien Moulin <julien@blueanchor.io>
2020-03-29 19:02:59 +0200

    feat(doctor-profile): separate if

--- lib/components/doctor_profile_page.dart
+++ lib/components/doctor_profile_page.dart
@@ -46,6 +46,7 @@ class DoctorProfilePage extends StatelessWidget {
                 validator: (value) {
                   Pattern pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
                   RegExp regex = new RegExp(pattern);
+
                   if (!regex.hasMatch(value)) {
                     return 'Veuillez entrer une adresse email valide';
                   }
