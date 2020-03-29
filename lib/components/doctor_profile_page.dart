31b7e1fc6a311eb3795a9f88a8bb895dad50573d
Julien Moulin <julien@blueanchor.io>
2020-03-30 00:05:18 +0200

    feat(doctor-profile): add style on inputs and labels

--- lib/components/doctor_profile_page.dart
+++ lib/components/doctor_profile_page.dart
@@ -14,22 +14,62 @@ class DoctorProfilePage extends StatelessWidget {
     Doctor doctor = new Doctor();
 
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 5, 82, 136),
-          title: Image.asset(
-            'assets/logo.png',
-            fit: BoxFit.contain,
-            height: 32
-          )
-        ),
-        body: Container(
-          color: const Color(0xFFF0F8FF),
-          padding: EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Nom'),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 5, 82, 136),
+        title: Image.asset(
+          'assets/logo.png',
+          fit: BoxFit.contain,
+          height: 32
+        )
+      ),
+      body: Container(
+        color: const Color(0xFFF0F8FF),
+        padding: EdgeInsets.all(38),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Vos coordonnées',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: const Color.fromARGB(255, 5, 82, 136),
+                  fontSize: 25,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    'Nom',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 5, 82, 136),
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(2),
+              ),
+              Container(
+                  height: 45,
+                  child:
+                    TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: const Color.fromARGB(255, 5, 82, 136), width: 2.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: const Color.fromARGB(35, 5, 82, 136), width: 2.0),
+                  ),
+                ),
                 validator: (value) {
                   if (value.isEmpty) {
                     return 'Veuillez entrer votre nom';
@@ -39,9 +79,40 @@ class DoctorProfilePage extends StatelessWidget {
 
                   return null;
                 }
+              )
+            ),
+              Padding(
+                padding: const EdgeInsets.all(10),
               ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Email'),
+              Row(
+                children: <Widget>[
+                  Text(
+                    'Email',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 5, 82, 136),
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(2),
+              ),
+              Container(
+                height: 45,
+                child:
+                TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: const Color.fromARGB(255, 5, 82, 136), width: 2.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: const Color.fromARGB(35, 5, 82, 136), width: 2.0),
+                  ),
+                ),
                 keyboardType: TextInputType.emailAddress,
                 validator: (value) {
                   Pattern pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
@@ -60,34 +131,70 @@ class DoctorProfilePage extends StatelessWidget {
                   return null;
                 }
               ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Numéro de mobile'),
-                keyboardType: TextInputType.phone,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Veuillez entrer un numéro de téléphone';
-                  }
-
-                  doctor.phone = value;
-
-                  return null;
-                }
               ),
-              RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    HttpService().createDoctor(doctor);
-
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Kanban()
+              Padding(
+                padding: const EdgeInsets.all(10),
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    'Numéro de téléphone',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 5, 82, 136),
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(2),
+              ),
+              Container(
+                  height: 45,
+                  child:
+                  TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: const Color.fromARGB(255, 5, 82, 136), width: 2.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: const Color.fromARGB(35, 5, 82, 136), width: 2.0),
+                        ),
                       ),
-                    );
-                  }
-                },
-                child: Text('Valider')
-              )
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Veuillez entrer votre nom';
+                        }
+
+                        doctor.phone = value;
+
+                        return null;
+                      }
+                  )
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+              ),
+
+              RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  HttpService().createDoctor(doctor);
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Kanban()
+                    ),
+                  );
+                }
+              },
+              child: Text('Valider')
+            )
             ]
           )
         )
